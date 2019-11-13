ebf1b62a7dd777250ef95f852b51ed0ce7c511e3
Leon-Avalos <heiko.avalos@gmail.com>
2019-11-13 00:25:14 -0500

    Bad request code and message added

--- ejercicio_5/routes/noticias.js
+++ ejercicio_5/routes/noticias.js
@@ -7,7 +7,7 @@ router.get('/noticias/:fecha/:categoria', (req, res) => {
     let fecha = req.params.fecha
     let categoria = req.params.fecha
     if(!fecha && !categoria){
-        res.status(404).sendFile(path.join(__dirname+'/404.html'))
+        res.status(400).sendFile(path.join(__dirname+'/404.html'))
         console.log("Paso por aca")
     }else{
         res.status(200).send({ruta: url, fecha: fecha, categoria: categoria})
